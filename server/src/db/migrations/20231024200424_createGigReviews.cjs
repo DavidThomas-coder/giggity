5c2773440c9a1206a78f4703526c59555409aa95
Solomon Montagno <sjmontagno@gmail.com>
2023-10-24 16:13:05 -0400

    Create migrations

---
+++ server/src/db/migrations/20231024200424_createGigReviews.cjs
@@ -0,0 +1,36 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+    return knex.schema.createTable('gigReviews', (table) => {
+        table.bigIncrements('id')
+        table.text('reviewBody')
+        table.integer('rating').notNullable()
+        table
+            .bigInteger('userId')
+            .unsigned()
+            .notNullable()
+            .index()
+            .references('users.id')
+        table
+            .bigInteger('gigId')
+            .unsigned()
+            .notNullable()
+            .index()
+            .references('gigs.id')
+        table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+        table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
+    })
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+        console.log(`Rolling back ${tableName}`);
+        return knex.schema.dropTableIfExists("gigReviews");
+}
