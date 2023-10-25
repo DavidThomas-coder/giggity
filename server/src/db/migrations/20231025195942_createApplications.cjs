ebc87ab52c9aa14e147e2df7f8adf04cb9cb0fc4
RipNasty <det625@gmail.com>
2023-10-25 16:10:49 -0400

    migration made

---
+++ server/src/db/migrations/20231025195942_createApplications.cjs
@@ -0,0 +1,35 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+    return knex.schema.createTable("applications", (table) => {
+        table.bigIncrements("id")
+        table
+            .bigInteger("userId")
+            .unsigned()
+            .references("users.id")
+            .index()
+            .notNullable()
+        table
+            .bigInteger("gigId")
+            .unsigned()
+            .references("gigs.id")
+            .index()
+            .notNullable()
+        table.text("applicationBody").notNullable()
+        table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now());
+        table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now());
+    })
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+    console.log(`Rolling back ${tableName}`);
+    return knex.schema.dropTableIfExists(tableName);
+}
