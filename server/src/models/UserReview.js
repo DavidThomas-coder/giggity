70ebcb24c67c595ffa70bc6f10ab6cf9ca7cf667
Todd Garrison <toddlgarrison@gmail.com>
2023-10-25 16:55:49 -0400

    add static get

--- server/src/models/UserReview.js
+++ server/src/models/UserReview.js
@@ -1,2 +1,22 @@
 const Model = require('./Model');
 
+class UserReview extends Model {
+    static get tableName() {
+        return 'userReviews';
+    }
+
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: ['rating'],
+            properties: {
+                rating: {type: ['integer', 'string']},
+                reviewBody: {type: 'string'},
+            }
+        }
+    }
+
+    static get relationMappings() {
+        const { User, Gig } = require('./index.js')
+    }
+}
