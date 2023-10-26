1a48ba25f5ef5b2588a6647c9513d51632186c64
RipNasty <det625@gmail.com>
2023-10-26 16:15:38 -0400

    asdfs

--- server/src/db/migrations/20231024202010_createUserReviews.cjs
+++ server/src/db/migrations/20231024202010_createUserReviews.cjs
@@ -21,6 +21,6 @@ exports.up = async (knex) => {
  * @param {Knex} knex
  */
 exports.down = (knex) => {
-  console.log(`Rolling back ${tableName}`);
+  // console.log(`Rolling back ${tableName}`);
   return knex.schema.dropTableIfExists("userReviews");
 };
