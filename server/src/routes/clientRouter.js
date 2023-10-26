1ec9ef4149626f286fd96aa83dbf2e5a2cf33f8c
Todd Garrison <toddlgarrison@gmail.com>
2023-10-26 16:40:01 -0400

    Update ClientRouter.js

--- server/src/routes/clientRouter.js
+++ server/src/routes/clientRouter.js
@@ -4,8 +4,17 @@ import getClientIndexPath from "../config/getClientIndexPath.js";
 const router = new express.Router();
 
 const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/gigs"];
+
 router.get(clientRoutes, (req, res) => {
   res.sendFile(getClientIndexPath());
+});
+
+router.get(authedClientRoutes, (req, res) => {
+  if (req.user) {
+    res.sendFile(getClientIndexPath());
+  } else {
+    res.redirect("/user-sessions/new")
+  }
 });
 
 export default router;
