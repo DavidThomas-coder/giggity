987b65bfd5b7d8220f7a64249e430c527801f825
RipNasty <det625@gmail.com>
2023-10-26 17:10:53 -0400

    232

--- server/src/routes/api/v1/userReviewsRouter.js
+++ server/src/routes/api/v1/userReviewsRouter.js
@@ -1,3 +1,12 @@
 import express from 'express';
 import objection from 'objection';
 import { UserReview } from '../../../models/index.js';
+
+const userReviewsRouter = new express.Router();
+
+userReviewsRouter.get('/', async (req, res) => {
+    try {
+        const userReviews = await UserReview.query()
+    }
+}
+    )
