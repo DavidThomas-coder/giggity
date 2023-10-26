f4799a0d1cd144f32279d83093eaa5591d792f5c
RipNasty <det625@gmail.com>
2023-10-26 17:13:58 -0400

    adding routers

--- server/src/routes/rootRouter.js
+++ server/src/routes/rootRouter.js
@@ -2,16 +2,21 @@ import express from "express";
 import userSessionsRouter from "./userSessionsRouter.js";
 import usersRouter from "./api/v1/usersRouter.js";
 import clientRouter from "./clientRouter.js";
+import gigReviewsRouter from "./api/v1/gigReviewsRouter.js";
+import gigsRouter from "./api/v1/gigsRouter.js";
+import userReviewsRouter from "./api/v1/userReviewsRouter.js";
 import router from "./clientRouter.js";
+
+
 const rootRouter = new express.Router();
 rootRouter.use("/", clientRouter);
 rootRouter.use("/api/v1/user-sessions", userSessionsRouter);
 rootRouter.use("/api/v1/users", usersRouter);
 
 //place your server-side routes here
-rootRouter.use('/api/v1/user-sessions', userSessionsRouter);
-rootRouter.use('/api/v1/users', usersRouter);
 rootRouter.use('/api/v1/gigs', gigsRouter);
 rootRouter.use('/api/v1/gigReviews', gigReviewsRouter)
+rootRouter.use('/api/v1/userReviews', userReviewsRouter)
+
 
 export default rootRouter;
