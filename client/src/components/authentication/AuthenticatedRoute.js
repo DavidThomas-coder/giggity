ad1b9bc673051ee64adbb04e23258b518e66a782
Todd Garrison <toddlgarrison@gmail.com>
2023-10-26 16:38:41 -0400

    update AuthenticatedRoute.js

--- client/src/components/authentication/AuthenticatedRoute.js
+++ client/src/components/authentication/AuthenticatedRoute.js
@@ -2,16 +2,20 @@ import React from "react";
 import { Redirect, Route } from "react-router";
 
 const AuthenticationCheck = ({ component: Component, user }) => {
+  if (user === undefined) {
+    return <div>Loading...</div>
+  }
   if (user !== null) {
-    return <Component />;
+    return <Component user={user} />;
   }
   return <Redirect to="/user-sessions/new" />;
 };
 
 const AuthenticatedRoute = ({ component, user, ...rest }) => {
+
   return (
     <Route
-      // eslint-disable-next-line react/jsx-props-no-spreading
+    
       {...rest}
     >
       <AuthenticationCheck user={user} component={component} />
