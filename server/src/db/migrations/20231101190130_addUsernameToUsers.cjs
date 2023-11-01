9243e02ec51eb687ce4f4d320c6ef58fad32f917
RipNasty <det625@gmail.com>
2023-11-01 15:07:56 -0400

    fixed parentheses

--- server/src/db/migrations/20231101190130_addUsernameToUsers.cjs
+++ server/src/db/migrations/20231101190130_addUsernameToUsers.cjs
@@ -6,11 +6,11 @@
  * @param {Knex} knex
  */
 exports.up = async (knex) => {
-    return knex.schema.alterTable("users"), (table) => {
+    return knex.schema.alterTable("users", (table) => {
         table.string("firstName").notNullable()
         table.string("lastName").notNullable()
         table.string("username").notNullable()
-    }
+    })
 }
 
 /**
