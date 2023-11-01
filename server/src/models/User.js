a4b8081dcb459046694245c31a1364b4b11469ec
Todd Garrison <toddlgarrison@gmail.com>
2023-11-01 14:30:08 -0400

    add properties

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -31,6 +31,9 @@ class User extends uniqueFunc(Model) {
       properties: {
         email: { type: "string", pattern: "^\\S+@\\S+\\.\\S+$" },
         cryptedPassword: { type: "string" },
+        address: { type: 'string' },
+        city: { type: 'string' },
+        
       },
     };
   }
