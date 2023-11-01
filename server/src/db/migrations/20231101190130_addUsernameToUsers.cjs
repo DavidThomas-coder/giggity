b2f671e5e2d2e03859ad213fa803308c954bee03
RipNasty <det625@gmail.com>
2023-11-01 15:05:28 -0400

    updated user model

---
+++ server/src/db/migrations/20231101190130_addUsernameToUsers.cjs
@@ -0,0 +1,25 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+    return knex.schema.alterTable("users"), (table) => {
+        table.string("firstName").notNullable()
+        table.string("lastName").notNullable()
+        table.string("username").notNullable()
+    }
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+    return knex.schema.table("users", (table) => {
+        table.dropColumn("firstName")
+        table.dropColumn("lastName")
+        table.dropColumn("username")
+})
+}
