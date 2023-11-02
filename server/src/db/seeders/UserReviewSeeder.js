07f326ef28f0e93d35fbfc7a0be77709d3e96a23
RipNasty <det625@gmail.com>
2023-11-02 14:54:42 -0400

    UserReviewSeeder made

--- server/src/db/seeders/UserReviewSeeder.js
+++ server/src/db/seeders/UserReviewSeeder.js
@@ -7,9 +7,30 @@ class UserReviewSeeder {
             {
                 reviewBody: "David slathered everywhere it ruled",
                 rating: "5",
-                gigId: 1,
+                gigId: 2,
                 userId: 1
+            },
+            {
+                reviewBody: "Todd is so ripped ez",
+                rating: "1",
+                gigId: 1,
+                userId: 2
+            },
+            {
+                reviewBody: "Why is Solomon's name Jimmy",
+                rating: "3",
+                gigId: 3,
+                userId: 3
             }
         ]
+
+        for (const singleUserReview of userReviewData) {
+            const currentUserReview = await UserReview.query().findOne({reviewBody: singleUserReview.reviewBody})
+            if (!currentUserReview) {
+                await UserReview.query().insert(singleUserReview)
+            }
+        }
     }
 }
+
+export default UserReviewSeeder
