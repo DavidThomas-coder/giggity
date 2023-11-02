783493d664612fab3cba13abf34fc27d5a95bafe
RipNasty <det625@gmail.com>
2023-11-02 14:50:05 -0400

    1

--- server/src/db/seeders/UserReviewSeeder.js
+++ server/src/db/seeders/UserReviewSeeder.js
@@ -1,2 +1,15 @@
 import { User, Gig, UserReview } from "../../models/index.js";
 
+class UserReviewSeeder {
+    static async seed() {
+
+        const userReviewData = [
+            {
+                reviewBody: "David slathered everywhere it ruled",
+                rating: "5",
+                gigId: 1,
+                userId: 1
+            }
+        ]
+    }
+}
