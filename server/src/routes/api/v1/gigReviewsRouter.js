eb52fc8a069b8660dc2085a56886ca42c4964e7f
RipNasty <det625@gmail.com>
2023-11-07 15:23:05 -0500

    fixed router

--- server/src/routes/api/v1/gigReviewsRouter.js
+++ server/src/routes/api/v1/gigReviewsRouter.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import objection from 'objection'
 import { GigReview } from '../../../models/index.js';
+import GigReviewSerializer from '../../../serializers/GigReviewSerializer.js';
 
 const gigReviewsRouter = new express.Router();
 
 gigReviewsRouter.get('/', async (req, res) => {
     try {
         const gigReviews = await GigReview.query()
-        res.status(200).json({ gigReviews: gigReviews })
+        const serializedGigReview = await GigReviewSerializer.showGigReviewDetails(gigReviews)
+        res.status(200).json({ gigReviews: serializedGigReview })
 
     } catch (error) {
         console.error(error);
