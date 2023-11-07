10568b967a15ac323398e7d752097931dabe4d93
Todd Garrison <toddlgarrison@gmail.com>
2023-11-07 15:49:34 -0500

    Create HomePage.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -28,7 +28,7 @@ const App = (props) => {
       <TopBar user={currentUser} />
       <Switch>
         <Route exact path="/">
-          <h2>Hello from react</h2>
+          <h2>Hello there</h2>
         </Route>
         <Route exact path="/users/new" component={RegistrationForm} />
         <Route exact path="/user-sessions/new" component={SignInForm} />
---
+++ client/src/components/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = (props) => {
+    return (
+        <h1>Hello from the homepage</h1>
+    )
+}
+
+export default HomePage;
