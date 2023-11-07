2aea62138a435b3ba7be26f0c81abab57c58c765
Solomon Montagno <sjmontagno@gmail.com>
2023-11-07 17:10:51 -0500

    Finally got Material UI to work

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+
 
 const HomePage = (props) => {
     return (
+      <div>
         <h1>Hello from the homepage</h1>
-    )
+        <div>
+          <Button variant="contained">Hello World</Button>
+        </div>
+      </div>
+    );
 }
 
 export default HomePage;
