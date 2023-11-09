de3f026fd6edd4ffbdfa876f6d99bd712ff75560
RipNasty <det625@gmail.com>
2023-11-09 15:10:07 -0500

    on homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import GigForm from './gigForm/GigForm.js';
 
 
 const HomePage = (props) => {
@@ -8,6 +9,9 @@ const HomePage = (props) => {
         <h1>Hello from the homepage</h1>
         <div>
           <Button variant="contained">Hello World</Button>
+        </div>
+        <div>
+          <GigForm />
         </div>
       </div>
     );
