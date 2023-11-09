c9d26f435f11ef93718320d3c1585e0d62212ddb
RipNasty <det625@gmail.com>
2023-11-09 14:51:37 -0500

    skeleton

--- client/src/components/gigForm/GigForm.js
+++ client/src/components/gigForm/GigForm.js
@@ -1,1 +1,11 @@
 import React, { useState, useEffect } from "react"
+
+const GigForm = () => {
+    return (
+        <div>
+            <form>
+                
+            </form>
+        </div>
+    )
+}
