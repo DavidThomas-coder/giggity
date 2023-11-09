ee8834f9fbf19f8ab45b6d231cc2808565c822de
Todd Garrison <toddlgarrison@gmail.com>
2023-11-09 16:23:23 -0500

    Remove button

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@mui/material/Button';
 import GigForm from './gigForm/GigForm.js';
 
 
@@ -7,9 +6,6 @@ const HomePage = (props) => {
     return (
       <div>
         <h1>Hello from the homepage</h1>
-        <div>
-          <Button variant="contained">Hello World</Button>
-        </div>
         <div>
           <GigForm />
         </div>
