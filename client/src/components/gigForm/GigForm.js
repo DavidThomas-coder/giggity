e8f6aaa430711e9b4ce06bed3265e98aca5ea082
RipNasty <det625@gmail.com>
2023-11-09 15:05:49 -0500

    fixes

--- client/src/components/gigForm/GigForm.js
+++ client/src/components/gigForm/GigForm.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from "react"
+import Button from '@mui/material/Button'
 
 const GigForm = () => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label>
-                    Post A Gig Here:
+                    Name:
+                        <input
+                            type="text"
+                            name="gigName"
+                            value={newGig.gigName}
+                            onChange={handleGigChange}
+                        />
+                </label>
+                <label>
+                    Description:
+                        <input
+                            type="text"
+                            name="gigName"
+                            value={newGig.gigName}
+                            onChange={handleGigChange}
+                        />
+                </label>
+                <label>
+                    
                         <input
                             type="text"
                             name="gigName"
@@ -58,24 +77,7 @@ const GigForm = () => {
                             onChange={handleGigChange}
                         />
                 </label>
-                <label>
-                    Post A Gig Here:
-                        <input
-                            type="text"
-                            name="gigName"
-                            value={newGig.gigName}
-                            onChange={handleGigChange}
-                        />
-                </label>
-                <label>
-                    Post A Gig Here:
-                        <input
-                            type="text"
-                            name="gigName"
-                            value={newGig.gigName}
-                            onChange={handleGigChange}
-                        />
-                </label>
+                <Button variant="contained" type="submit">Submit Gig</Button>
             </form>
         </div>
     )
