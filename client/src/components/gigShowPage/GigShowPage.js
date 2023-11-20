debea2b3bef5bb0a7f48353e0bcb7cd71dbbb783
Todd Garrison <toddlgarrison@gmail.com>
2023-11-20 16:08:02 -0500

    create gigs.context.jsx

--- client/src/components/gigShowPage/GigShowPage.js
+++ client/src/components/gigShowPage/GigShowPage.js
@@ -1,4 +1,11 @@
 import './gigShowPage.styles.scss'
 import React from "react";
+import GigForm from '../gigForm/GigForm';
 
+const GigShowPage = (props) => {
+    return (
+        <GigForm />
+    )
+}
 
+export default GigShowPage
