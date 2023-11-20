98bbe6493612de623d28f8c91b9de13b1a664c01
Todd Garrison <toddlgarrison@gmail.com>
2023-11-20 16:12:01 -0500

    add children

---
+++ client/src/context/gigs.context.jsx
@@ -0,0 +1,10 @@
+import { createContext, useState, useEffect } from "react";
+
+export const GigContext = createContext({
+    currentGig: null,
+    setCurrentGig: () => null
+})
+
+export const GigProvider = ({ children }) => {
+    const [currentGig, setCurrentGig] = useState(null)
+}
