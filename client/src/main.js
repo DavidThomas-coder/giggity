52f949d1e2d11963798857305890193514b2193b
Todd Garrison <toddlgarrison@gmail.com>
2023-11-20 16:33:28 -0500

    Add fix

--- client/src/main.js
+++ client/src/main.js
@@ -4,29 +4,33 @@ import { render } from "react-dom";
 import App from "./components/App";
 import config from "./config";
 import RedBox from "redbox-react";
-import { GigProvider } from "./context/gigs.context.js";
+import { GigProvider } from "./context/gigs.context";
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   let reactElement = document.getElementById("app");
+document.addEventListener("DOMContentLoaded", () => {
+  let reactElement = document.getElementById("app");
 
-//   if (reactElement) {
-//     if (config.nodeEnv === "development") {
-//       try {
-//         render(<App />, reactElement);
-//       } catch (e) {
-//         render(<RedBox error={e} />, reactElement);
-//       }
-//     } else {
-//       render(<App />, reactElement);
-//     }
-//   }
-//});
+  if (reactElement) {
+    if (config.nodeEnv === "development") {
+      try {
+        <GigProvider>
+          render(<App />, reactElement);
+        </GigProvider>
+      } catch (e) {
+        <GigProvider>
+          render(<RedBox error={e} />, reactElement);
+        </GigProvider>
+      }
+    } else {
+      render(<App />, reactElement);
+    }
+  }
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <GigProvider>
-      <App />
-    </GigProvider>
-  </React.StrictMode>
-)
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <React.StrictMode>
+//     <GigProvider>
+//       <App />
+//     </GigProvider>
+//   </React.StrictMode>
+// )
