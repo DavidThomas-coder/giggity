6c20d40c55bda9f8018caad5c5d21a685f4a3f41
RipNasty <det625@gmail.com>
2023-11-29 15:30:48 -0500

    2

--- client/src/components/userProfile/userProfile.js
+++ client/src/components/userProfile/userProfile.js
@@ -1,2 +1,15 @@
-import "./userProfile.styles.scss"
+import "./UserProfile.styles.scss"
 
+import React, {useEffect, useState} from "react"
+
+const UserProfile = ({ user }) => {
+
+    return (
+        <div>
+            <h1>User Profile</h1>
+
+        </div>
+    )
+}
+
+export default UserProfile
