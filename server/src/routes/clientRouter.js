6c1afe6de47e3656edc0603770fc51da9503ef3d
Todd Garrison <toddlgarrison@gmail.com>
2023-11-29 15:57:28 -0500

    add to clientRouter

--- server/src/routes/clientRouter.js
+++ server/src/routes/clientRouter.js
@@ -3,7 +3,7 @@ import getClientIndexPath from "../config/getClientIndexPath.js";
 
 const router = new express.Router();
 
-const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/gigs", "/gigReviews", "/userReviews"];
+const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/gigs", "/gigReviews", "/userReviews", "/gigs/:id"];
 
 const authedClientRoutes = ["/profile"];
 
