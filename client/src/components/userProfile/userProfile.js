00f31a45fcd2e211afa72f989cf8947b97bab3c6
Todd Garrison <toddlgarrison@gmail.com>
2023-11-29 16:08:18 -0500

    Set up getPostedGigs

--- client/src/components/userProfile/userProfile.js
+++ client/src/components/userProfile/userProfile.js
@@ -4,7 +4,13 @@ import React, {useEffect, useState} from "react"
 
 const UserProfile = ({ user }) => {
 
-
+    const getPostedGigs = async () => {
+        try {
+            const response = await fetch("/api/v1/profile/gigs")
+        } catch (error) {
+            
+        }
+    }
 
     return (
         <div>
