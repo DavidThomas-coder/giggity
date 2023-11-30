cece5e57b9d097a8d1a5c7d9bbb931d21f8bdcc4
Solomon Montagno <sjmontagno@gmail.com>
2023-11-30 15:28:15 -0500

    Fixed some server side issues

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,11 +3,12 @@ import GigForm from './gigForm/GigForm.js';
 
 
 const HomePage = (props) => {
+
+
     return (
       <div>
         <h1>Hello from the homepage</h1>
         <div>
-          <GigForm />
         </div>
       </div>
     );
