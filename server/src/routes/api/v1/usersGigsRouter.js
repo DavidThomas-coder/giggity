de3eb45cefb68fd8f57cf9687d4aa53e215bc844
Todd Garrison <toddlgarrison@gmail.com>
2023-12-07 15:38:01 -0500

    finished router

--- server/src/routes/api/v1/usersGigsRouter.js
+++ server/src/routes/api/v1/usersGigsRouter.js
@@ -4,3 +4,22 @@ import { Gig, User } from "../../../models/index.js"
 
 const usersGigsRouter = new express.Router();
 
+usersGigsRouter.get('/:id/gigs', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        //find gigs where the owner matches the user's id
+        const ownedGigs = await Gig.query().where('ownerId', id)
+
+        const serializedOwnedGigs = ownedGigs.map((gig) => 
+            GigSerializer.showGigDetails(gig, req.user))
+        
+        return res.status(200).json({
+            ownedGigs: serializedOwnedGigs
+        })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+})
+
+export default usersGigsRouter
