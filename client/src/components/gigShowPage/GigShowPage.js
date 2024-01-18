c65e6ce0326b792f6feec394ab3226cd34166425
RipNasty <det625@gmail.com>
2024-01-18 12:44:42 -0500

    fleshing out gigShow

--- client/src/components/gigShowPage/GigShowPage.js
+++ client/src/components/gigShowPage/GigShowPage.js
@@ -1,8 +1,14 @@
 import './gigShowPage.styles.scss'
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GigForm from '../gigForm/GigForm';
 
 const GigShowPage = (props) => {
+    const [gigShow, setGigShow] = useState({
+        id: ""
+    })
+
+    const getGig = async
+
     return (
         <GigForm />
     )
