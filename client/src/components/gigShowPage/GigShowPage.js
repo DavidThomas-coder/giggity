e5cd123e968b51ebcb8293e6af68ce7566e45405
RipNasty <det625@gmail.com>
2024-01-18 12:50:01 -0500

    return

--- client/src/components/gigShowPage/GigShowPage.js
+++ client/src/components/gigShowPage/GigShowPage.js
@@ -1,16 +1,33 @@
 import './gigShowPage.styles.scss'
 import React, { useState, useEffect } from "react";
-import GigForm from '../gigForm/GigForm';
+// import GigForm from '../gigForm/GigForm';
 
 const GigShowPage = (props) => {
     const [gigShow, setGigShow] = useState({
         id: ""
     })
 
-    const getGig = async
+    const getGig = async () => {
+        const gigId = props.match.params.id;
+        try {
+        const response = await fetch(`/api/v1/gigs/${gigId}`);
+        if (!response.ok) {
+            const errorMessage = `${response.status} (${response.statusText})`;
+            const error = new Error(errorMessage);
+            throw error;
+        }
+        const body = await response.json();
+        setGigShow(body.gig);
+        } catch (err) {
+        console.error(`Error in fetch: ${err.message}`);
+        }
+    };
 
     return (
-        <GigForm />
+        <div>
+            <h1>{gigShow.gigName}</h1>
+            <p>{gigShow.description}</p>
+        </div>
     )
 }
 
