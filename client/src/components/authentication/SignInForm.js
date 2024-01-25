15aabab5820592703ffe8a1a31002c029e20b0a3
Solomon Montagno <sjmontagno@gmail.com>
2024-01-25 13:35:59 -0500

    add MU sign in component (#35)

--- client/src/components/authentication/SignInForm.js
+++ client/src/components/authentication/SignInForm.js
@@ -1,110 +1,229 @@
-import React, { useState } from "react";
-import config from "../../config";
-import FormError from "../layout/FormError";
+// import React, { useState } from "react";
+// import config from "../../config";
+// import FormError from "../layout/FormError";
 
-const SignInForm = () => {
-  const [userPayload, setUserPayload] = useState({ email: "", password: "" });
-  const [shouldRedirect, setShouldRedirect] = useState(false);
-  const [errors, setErrors] = useState({});
-  const [credentialsErrors, setCredentialsErrors] = useState("")
+// const SignInForm = () => {
+//   const [userPayload, setUserPayload] = useState({ email: "", password: "" });
+//   const [shouldRedirect, setShouldRedirect] = useState(false);
+//   const [errors, setErrors] = useState({});
+//   const [credentialsErrors, setCredentialsErrors] = useState("")
 
-  const validateInput = (payload) => {
-    setErrors({});
-    const { email, password } = payload;
-    const emailRegexp = config.validation.email.regexp.emailRegex;
-    let newErrors = {};
-    if (!email.match(emailRegexp)) {
-      newErrors = {
-        ...newErrors,
-        email: "is invalid",
-      };
-    }
+//   const validateInput = (payload) => {
+//     setErrors({});
+//     const { email, password } = payload;
+//     const emailRegexp = config.validation.email.regexp.emailRegex;
+//     let newErrors = {};
+//     if (!email.match(emailRegexp)) {
+//       newErrors = {
+//         ...newErrors,
+//         email: "is invalid",
+//       };
+//     }
 
-    if (password.trim() === "") {
-      newErrors = {
-        ...newErrors,
-        password: "is required",
-      };
-    }
+//     if (password.trim() === "") {
+//       newErrors = {
+//         ...newErrors,
+//         password: "is required",
+//       };
+//     }
 
-    setErrors(newErrors);
-    if (Object.keys(newErrors).length === 0) {
-      return true
-    }
-    return false
-  };
+//     setErrors(newErrors);
+//     if (Object.keys(newErrors).length === 0) {
+//       return true
+//     }
+//     return false
+//   };
 
-  const onSubmit = async (event) => {
-    event.preventDefault()
-    if (validateInput(userPayload)) {
-      try {
-        if (Object.keys(errors).length === 0) {
-          const response = await fetch("/api/v1/user-sessions", {
-            method: "post",
-            body: JSON.stringify(userPayload),
-            headers: new Headers({
-              "Content-Type": "application/json",
-            })
-          })
-          if(!response.ok) {
-            if (response.status === 401) {
-              const serverErrors = await response.json()
-              setCredentialsErrors(serverErrors.message)
-            }
-            const errorMessage = `${response.status} (${response.statusText})`
-            const error = new Error(errorMessage)
-            throw(error)
-          }
-          const userData = await response.json()
-          setShouldRedirect(true)
-        }
-      } catch(err) {
-        console.error(`Error in fetch: ${err.message}`)
-      }
-    }
-  }
+//   const onSubmit = async (event) => {
+//     event.preventDefault()
+//     if (validateInput(userPayload)) {
+//       try {
+//         if (Object.keys(errors).length === 0) {
+//           const response = await fetch("/api/v1/user-sessions", {
+//             method: "post",
+//             body: JSON.stringify(userPayload),
+//             headers: new Headers({
+//               "Content-Type": "application/json",
+//             })
+//           })
+//           if(!response.ok) {
+//             if (response.status === 401) {
+//               const serverErrors = await response.json()
+//               setCredentialsErrors(serverErrors.message)
+//             }
+//             const errorMessage = `${response.status} (${response.statusText})`
+//             const error = new Error(errorMessage)
+//             throw(error)
+//           }
+//           const userData = await response.json()
+//           setShouldRedirect(true)
+//         }
+//       } catch(err) {
+//         console.error(`Error in fetch: ${err.message}`)
+//       }
+//     }
+//   }
 
-  const onInputChange = (event) => {
-    setUserPayload({
-      ...userPayload,
-      [event.currentTarget.name]: event.currentTarget.value,
+//   const onInputChange = (event) => {
+//     setUserPayload({
+//       ...userPayload,
+//       [event.currentTarget.name]: event.currentTarget.value,
+//     });
+//   };
+
+//   if (shouldRedirect) {
+//     location.href = "/";
+//   }
+
+//   return (
+//     <div className="grid-container">
+//       <h1>Sign In</h1>
+//       {credentialsErrors ? <p className="callout alert">{credentialsErrors}</p> : null}
+//       <form onSubmit={onSubmit}>
+//         <div>
+//           <label>
+//             Email
+//             <input type="text" name="email" value={userPayload.email} onChange={onInputChange} />
+//             <FormError error={errors.email} />
+//           </label>
+//         </div>
+//         <div>
+//           <label>
+//             Password
+//             <input
+//               type="password"
+//               name="password"
+//               value={userPayload.password}
+//               onChange={onInputChange}
+//             />
+//             <FormError error={errors.password} />
+//           </label>
+//         </div>
+//         <div>
+//           <input type="submit" className="button" value="Sign In" />
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Giggity
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function SignInForm() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
     });
   };
 
-  if (shouldRedirect) {
-    location.href = "/";
-  }
-
   return (
-    <div className="grid-container">
-      <h1>Sign In</h1>
-      {credentialsErrors ? <p className="callout alert">{credentialsErrors}</p> : null}
-      <form onSubmit={onSubmit}>
-        <div>
-          <label>
-            Email
-            <input type="text" name="email" value={userPayload.email} onChange={onInputChange} />
-            <FormError error={errors.email} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input
-              type="password"
-              name="password"
-              value={userPayload.password}
-              onChange={onInputChange}
-            />
-            <FormError error={errors.password} />
-          </label>
-        </div>
-        <div>
-          <input type="submit" className="button" value="Sign In" />
-        </div>
-      </form>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                </Grid>
+                <Grid item>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
-};
+}
 
-export default SignInForm;
+// export default SignInForm;
