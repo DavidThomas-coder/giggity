4ecbb839de94de675ab5d2a7fbf7a735b50d1d6f
Solomon Montagno <sjmontagno@gmail.com>
2024-01-25 13:35:03 -0500

    add MU sign in component

--- server/babel.config.cjs
+++ server/babel.config.cjs
@@ -1,3 +1,15 @@
-module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]]
-};
+module.exports ={
+  "presets": [
+    "@babel/preset-react",
+    [
+      "@babel/preset-env",
+      {
+        "modules": false, // Disable module transformation for ESM support
+        "targets": {
+          "browsers": ["last 2 versions", "not dead", "not < 2%"],
+          "node": "current"
+        }
+      }
+    ]
+  ]
+}
